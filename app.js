0808e6c95837cd2669f960b9f7930d3e1a1a96df
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-10-24 13:15:30 -0400

    omg

---
+++ app.js
@@ -0,0 +1,18 @@
+const DOMselectors = {
+button : document.getElementById("BTN"),
+box: document.getElementById("CTN-BOX"),
+input: document.getElementById("input")
+}
+
+
+
+DOMselectors.button.addEventListener("click" , function(){
+    let input = DOMselectors.input.value;
+    DOMselectors.box.insertAdjacentElement("afterend", <p>`${input}`</p>)
+
+    
+
+
+
+})
+
