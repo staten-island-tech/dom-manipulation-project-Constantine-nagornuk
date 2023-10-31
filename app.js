bd1d7395cde56f0c30ff8ab17333d8aeb754de3f
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-10-31 13:14:36 -0400

    FIX IT

--- app.js
+++ app.js
@@ -8,41 +8,59 @@
         delete: document.querySelector("#Delete")
     }
 
+
+
+    function clear(){
+ DOMselectors.set.value= "";
+ DOMselectors.image.value= "";
+ DOMselectors.title.value= "";}
+
+
+function deel(x)
+{DOMselectors.box.addEventListener('click', function(event) {
+    if (event.target.classList.contains(x)) {
+      event.target.parentElement.remove();
+    }
+  });
+}
+
+
+function inject(x){DOMselectors.box.insertAdjacentHTML('beforeend', x);}
+
+
 DOMselectors.sumbit.addEventListener("click", function(e){
     e.preventDefault()
 
 
-    let x = DOMselectors.set.value
-    let z = DOMselectors.image.value
-    let y = DOMselectors.title.value
-    if (y &&  z && x) {
+    let poop1 = DOMselectors.set.value
+    let poop2 = DOMselectors.image.value
+    let poop3 = DOMselectors.title.value
+    if (poop1 &&  poop2 && poop3) {
       const cardHTML = `
         <div class="card">
-          <h2 class="Lego#">${y}</h2>
-          <img class="Image" src="${z}" alt="${y}">
-          <p id="number_of_set">${x}</p>
+          <h2 class="Lego#">${poop3}</h2>
+          <img class="Image" src="${poop2}" alt="${poop1}">
+          <p id="number_of_set">${poop1}</p>
           <button class="Delete">Delete</button>
         </div>
       `
-  
-      DOMselectors.box.insertAdjacentHTML('beforeend', cardHTML);
-  
+      
+        inject(cardHTML)
+        clear()
      
-      DOMselectors.title.value = "";
-      DOMselectors.image.value = "";
-      DOMselectors.set.value = "";
     } else {
       alert('you did not put anything in the boxes poopster!');
     }
   });
 
-  DOMselectors.box.addEventListener('click', function(event) {
-    if (event.target.classList.contains('Delete')) {
-      event.target.parentElement.remove();
-    }
-  });
 
 
+deel('Delete')
+
+
+
+
+    
 
 
 
