3ee3edc8371fbe8c97ef498ecf26d3ebc82009d5
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-10-30 20:27:32 -0400

    i think i am done but need to make look pretty now

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
         set: document.querySelector("#Set_Number"),
         image: document.querySelector("#Image"),
         title: document.querySelector('#Title'),
-      box: document.getElementById("CTN")
+        box: document.getElementById("CTN"),
+        delete: document.querySelector("#Delete")
     }
 
 DOMselectors.sumbit.addEventListener("click", function(e){
@@ -14,7 +15,35 @@ DOMselectors.sumbit.addEventListener("click", function(e){
     let x = DOMselectors.set.value
     let z = DOMselectors.image.value
     let y = DOMselectors.title.value
-    DOMselectors.box.insertAdjacentHTML("beforeend", `<p> ${ x} , ${z} , ${y}</p>` )
+    if (y &&  z && x) {
+      const cardHTML = `
+        <div class="card">
+          <h2 class="Lego#">${y}</h2>
+          <img class="Image" src="${z}" alt="${y}">
+          <p id="number_of_set">${x}</p>
+          <button class="Delete">Delete</button>
+        </div>
+      `
+  
+      DOMselectors.box.insertAdjacentHTML('beforeend', cardHTML);
+  
+     
+      DOMselectors.title.value = "";
+      DOMselectors.image.value = "";
+      DOMselectors.set.value = "";
+    } else {
+      alert('you did not put anything in the boxes poopster!');
+    }
+  });
+
+  DOMselectors.box.addEventListener('click', function(event) {
+    if (event.target.classList.contains('Delete')) {
+      event.target.parentElement.remove();
+    }
+  });
 
 
-}) 
+
+
+
+
